d0137ff35e2a972066e7fc67e5223e63e3e0501a
duancanghai <74281791+duancanghai@users.noreply.github.com>
2020-11-13 17:32:34 +0800

    Add files via upload

---
+++ file/hunqing.ip3q.com/skin/hunqing/js/sliders.js
@@ -0,0 +1,17 @@
+window.onload = function ()
+{
+	var oLi = document.getElementById("tab").getElementsByTagName("li");
+	var oUl = document.getElementById("content").getElementsByTagName("ul");
+	
+	for(var i = 0; i < oLi.length; i++)
+	{
+		oLi[i].index = i;
+		oLi[i].onmouseover = function ()
+		{
+			for(var n = 0; n < oLi.length; n++) oLi[n].className="";
+			this.className = "current";
+			for(var n = 0; n < oUl.length; n++) oUl[n].style.display = "none";
+			oUl[this.index].style.display = "block"
+		}	
+	}
+}
