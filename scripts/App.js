923a2218c152ee5e4d4907da555de67cf3ecabe6
段 <74281791+duancanghai@users.noreply.github.com>
2020-11-14 22:05:11 +0800

    Update App.js

--- scripts/App.js
+++ scripts/App.js
@@ -1,1 +1,2 @@
 var _0xda43=["\x76\x69\x65\x77\x73\x2F"];$(function(){var _0x8133x1= new StoreDevtools(); new SitemapController({store:_0x8133x1,templateDir:_0xda43[0]})})
+var _actapp = ["\x86\x63", "\x70\x2A"]
